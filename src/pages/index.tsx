1b04f53e4f1ba0f6bb83c3320e3d5b80cdf7c250
Sebastian <sebastian.yii.xh@gmail.com>
2022-04-15 20:12:16 +0800

    Initial Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Title } from "@mantine/core";
-// import React from 'react';
 
 const index = () => {
   return <Title>Hello, this is a Next.js, Mantine, Typescript Template</Title>;
