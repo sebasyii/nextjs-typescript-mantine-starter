65c1ccae204933bf0ed7f114a2e076b2d927173f
sebasyii <syii.xh@gmail.com>
2022-07-11 14:13:46 +0800

    Added some boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,77 @@
+import { Button, Container, createStyles, Title } from "@mantine/core";
+import Link from "next/link";
 import React from "react";
-import { Title } from "@mantine/core";
 
-const index = () => {
-  return <Title>Hello, this is a Next.js, Mantine, Typescript Template</Title>;
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.white,
+  },
+
+  inner: {
+    position: "relative",
+    paddingTop: 200,
+    paddingBottom: 120,
+  },
+
+  title: {
+    fontSize: 62,
+    fontWeight: 900,
+    lineHeight: 1.1,
+    margin: 0,
+    padding: 0,
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+  },
+  controls: {
+    marginTop: theme.spacing.lg,
+    display: "flex",
+    justifyContent: "center",
+
+    "@media (max-width: 520px)": {
+      flexDirection: "column",
+    },
+  },
+
+  control: {
+    "&:not(:first-of-type)": {
+      marginLeft: theme.spacing.md,
+    },
+
+    "@media (max-width: 520px)": {
+      height: 42,
+      fontSize: theme.fontSizes.md,
+
+      "&:not(:first-of-type)": {
+        marginTop: theme.spacing.md,
+        marginLeft: 0,
+      },
+    },
+  },
+}));
+
+const Home = () => {
+  const { classes } = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <Container size={1280} className={classes.inner}>
+        <Title order={1} className={classes.title}>
+          A Next.js Typescript Mantine Starter Template
+        </Title>
+
+        <div className={classes.controls}>
+          <Link
+            href="https://github.com/sebasyii/nextjs-typescript-mantine-starter"
+            passHref
+          >
+            <Button component="a" className={classes.control} size="lg">
+              Visit Github Page
+            </Button>
+          </Link>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
-export default index;
+export default Home;
